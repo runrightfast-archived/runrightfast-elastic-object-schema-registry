d5355ddd07afc12a7704b7244e2dc0d69e59dc21
Alfio Zappala <azappala@azaptree.com>
2013-10-29 23:45:27 -0400

    added support for managing the index

--- test/object-schema-registry-database.js
+++ test/object-schema-registry-database.js
@@ -41,10 +41,28 @@ describe('database', function() {
 
 	var idsToDelete = [];
 
+	before(function(done){
+		database.database.deleteIndex()
+		.then(function(result){
+			console.log('DELETED INDEX: ' + JSON.stringify(result,undefined,2));
+			database.database.createIndex({})				
+				.then(function(){
+					console.log('CREATED INDEX: ' + JSON.stringify(result,undefined,2));
+					database.setMapping().						
+						then(function(result){
+							console.log('SET MAPPING: ' + JSON.stringify(result,undefined,2));
+							done();
+						},done);
+				},done)
+		},done)
+	});
+
 	afterEach(function(done) {
 		database.deleteObjectSchemas(idsToDelete).then(function(result) {
 			idsToDelete = [];
-			done();
+			database.database.refreshIndex().then(function(){
+				done();
+			});			
 		}, function(error) {
 			console.error(JSON.stringify(error, undefined, 2));
 			done(error.error);
@@ -57,9 +75,9 @@ describe('database', function() {
 			version : '1.0.0',
 			description : 'Couchbase config schema'
 		});
+		idsToDelete.push(schema.id);
 		database.createObjectSchema(schema).then(function(result) {
 			console.log(JSON.stringify(result, undefined, 2));
-			idsToDelete.push(result._id);
 			done();
 		}, function(err) {
 			console.error('create failed : ' + err);
@@ -73,14 +91,45 @@ describe('database', function() {
 			version : '1.0.0',
 			description : 'Couchbase config schema'
 		});
+		idsToDelete.push(schema.id);
+
 		database.createObjectSchema(schema).then(function(result) {
 			console.log('create response: ' + JSON.stringify(result, undefined, 2));
-			idsToDelete.push(result._id);
-
 			database.getObjectSchema(result._id).then(function(result) {
 				console.log('retrieved : ' + JSON.stringify(result, undefined, 2));
+				//validates that the returned object is a valid ObjectSchema
 				var objectSchema = new ObjectSchema(result._source);
+				console.log('objectSchema : ' + JSON.stringify(result, undefined, 2));
 				done();
+			}, done);
+
+		}, function(err) {
+			console.error('create failed : ' + err);
+			done(err);
+		});
+	});
+
+	it('can find an ObjectSchema by its namespace and version',function(done){
+		var schema = new ObjectSchema({
+			namespace : 'ns://runrightfast.co/couchbase',
+			version : '1.0.0',
+			description : 'Couchbase config schema'
+		});
+		idsToDelete.push(schema.id);
+
+		database.createObjectSchema(schema).then(function(result) {
+			console.log('create response: ' + JSON.stringify(result, undefined, 2));
+
+			database.findByNamespaceVersion(schema.namespace,schema.version).then(function(result) {
+				console.log('findByNamespaceVersion result : ' + JSON.stringify(result, undefined, 2));
+				try{
+					expect(result.hits.total).to.equal(1);
+					//validates that the returned object is a valid ObjectSchema
+					var objectSchema = new ObjectSchema(result.hits.hits[0]._source);
+					done();
+				}catch(err){
+					done(err);
+				}
 			}, done);
 
 		}, function(err) {
